260d9bb61167a34fdf741ad7d24f1588d9f94944
PengfanZ <pz723@nyu.edu>
2021-10-25 18:36:26 -0400

    wip

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,10 @@
 // import logo from "./logo.svg";
-// import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import CreateAccount from "./pages/CreateAccount";
 
 function App() {
   return (
@@ -17,11 +17,14 @@ function App() {
             <Header />
             <Login />
           </Route>
-    
+          <Route path='/create-account'>
+            <Header />
+            <CreateAccount />
+          </Route>
           <Route path='/'>
             <Header />
-            <Home /> 
-            </Route>
+            <Home />
+          </Route>
         </Switch>
       </Router>
     </div>
