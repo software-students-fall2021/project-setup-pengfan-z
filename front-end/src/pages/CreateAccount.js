f6472e3dd781e1aba7e0bfac83631604e769012c
PengfanZ <pz723@nyu.edu>
2021-10-25 23:57:40 -0400

    Add create account page; add fake api for login page; add module axios

--- front-end/src/pages/CreateAccount.js
+++ front-end/src/pages/CreateAccount.js
@@ -1,14 +1,43 @@
 import { useState } from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Container, Row, Col, Alert } from "react-bootstrap";
 import "../css/createAccount.css";
 
 const CreateAccount = () => {
   const [newUserName, setUserName] = useState("");
   const [newPassword, setPassWord] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+  const [alert, setAlert] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = () => {
+    if (newPassword !== repeatedPassword) {
+      setAlert(
+        "Your reentered passwords does not match your original passwords."
+      );
+      setShowAlert(true);
+    } else if (
+      newUserName === "" ||
+      newPassword === "" ||
+      repeatedPassword === ""
+    ) {
+      setAlert("Every field is required.");
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  };
+
   return (
     <div>
       <h4 className='title'>Create Account</h4>
+      <Alert
+        variant='danger'
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        dismissible
+      >
+        <p>{alert}</p>
+      </Alert>
       <Container fluid className='form justify-content-top'>
         <Row>
           <Col>
@@ -40,10 +69,26 @@ const CreateAccount = () => {
         </Row>
         <Row>
           <Col>
-            <Button className='button' variant='outline-primary'>
-              <Link to='/create-account' className='navLink'>
-                Create Account
-              </Link>
+            <label className='input'>Confirm Password: </label>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <input
+              type='password'
+              onChange={(event) => setRepeatedPassword(event.target.value)}
+              className='input'
+            ></input>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              className='button'
+              variant='outline-primary'
+              onClick={handleSubmit}
+            >
+              Submit
             </Button>
           </Col>
         </Row>
--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,20 +1,69 @@
 import { useState } from "react";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col, Alert } from "react-bootstrap";
 import "../css/login.css";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
 
 function Login() {
   const [username, setUserName] = useState("");
   const [password, setPassWord] = useState("");
+  const [alert, setAlert] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [data, setData] = useState([]);
 
   const handleLogin = () => {
-    console.log(`Username: ${username}; Password: ${password}`);
+    console.log(data);
+    if (username === "" || password === "") {
+      setAlert("Every field is required.");
+      setShowAlert(true);
+    } else if (!authentication()) {
+      setAlert("Account or password is wrong.");
+      setShowAlert(true);
+    } else {
+      console.log(`Username: ${username}; Password: ${password}`);
+      // Should redirect to account page
+      <Redirect to='/' />;
+    }
   };
+
+  const authentication = () => {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].account === username) {
+        if (data[i].password === password) {
+          console.log(password);
+          console.log(data[i].password);
+          return true;
+        }
+        return false;
+      }
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      // mockaroo api
+      const result = await axios(
+        "https://my.api.mockaroo.com/login?key=52caca70"
+      );
+      setData(result.data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       {/* <h2>{password}</h2> */}
       {/* <form> */}
+      <Alert
+        variant='danger'
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        dismissible
+      >
+        <p>{alert}</p>
+      </Alert>
       <Container fluid className='form justify-content-center'>
         <Row>
           <Col>
