45b006e8043b33fc7223af074444dbc11b979b92
Lianne Yaacoby <lianneyaacoby@Liannes-MBP.lan1>
2021-11-07 21:35:46 -0500

    Back-end changes to login route

---
+++ back-end/public/teamPasswords.js
@@ -0,0 +1,39 @@
+// Create dummy data to simulate storing login & password info on database (Sprint 3)
+const account = {
+    one: { 
+         "id": 1, 
+         "accountName": "lyaacoby", 
+         "accountPassword": "lianne"
+     },
+ 
+    two: {
+         "id": 2, 
+         "accountName": "pzhang", 
+         "accountPassword": "pengfan"
+     },
+ 
+     three: {
+         "id": 3, 
+         "accountName": "amadsen", 
+         "accountPassword": "avi"
+     },
+ 
+    four: {
+         "id": 4, 
+         "accountName": "lli", 
+         "accountPassword": "linjian"
+     },
+ 
+    five: {
+         "id": 5,
+         "accountName": "yfan", 
+         "accountPassword": "yuang"
+     },
+     
+     six: {
+         "id": 6,
+         "accountName": "hhans", 
+         "accountPassword": "hans"
+     }
+ }
+ module.exports = account;
