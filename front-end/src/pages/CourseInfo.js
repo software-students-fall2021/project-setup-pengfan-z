37e7a31a3de2878ea0be68f1bbe4eef7d1f47eea
PengfanZ <pz723@nyu.edu>
2021-11-09 18:33:16 -0500

    wip

--- front-end/src/pages/CourseInfo.js
+++ front-end/src/pages/CourseInfo.js
@@ -1,134 +1,154 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { useParams, useHistory, Link } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
+import { useParams, useHistory, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../css/courseInfo.css";
-
-const UserReview = props => {
-    return (
-        <div>
-            <Row>
-                <Col>
-                    <p className='text-start'>{props.details.username}</p>
-                </Col>
-                <Col>
-                    <p className='text-end'>Rating: {props.details.rating}</p>
-                </Col>
-            </Row>
-            <Row className='text-center'>
-                <Col>
-                    <p>{props.details.comment}</p>
-                </Col>
-            </Row>
-        </div>
-    );
-}
+import UserReview from "../components/UserReview";
 
 const CourseInfo = () => {
-    const { schoolId, subjectId, courseId } = useParams();
-    const [courseInfo, setCourseInfo] = useState();
-    const [showMore, setShowMore] = useState(false);
-    const history = useHistory()
+  const { schoolId, subjectId, courseId } = useParams();
+  const [courseInfo, setCourseInfo] = useState();
+  const [showMore, setShowMore] = useState(false);
+  const history = useHistory();
 
-    const fetchData = async() => {
-        let response = await axios(
-            `/courses/${schoolId}/${subjectId}`
-        );
+  const fetchData = async () => {
+    let response = await axios(`/courses/${schoolId}/${subjectId}`);
 
-        let data = response.data.filter(course => course.deptCourseId == courseId);
-        if (data.length === 0) {
-            history.push(`/school/${schoolId}/${subjectId}`);
-            return null;
-        }
-        data = data[0]
-        if (data.sections.length > 0) {
-            data.prerequisite = data.sections[0].prerequisite;
-        }
-
-        console.log(data);
-
-        response = await axios(`/comments/${courseId}`);
-        data.userReviews = response.data;
-        data.avgRating = 0
-        if (data.userReviews.length > 0) {
-            data.avgRating = Math.round(data.userReviews.reduce((sum, review) => sum + review.rating, 0) / data.userReviews.length);
-        }
-
-        return data;
+    let data = response.data.filter(
+      (course) => course.deptCourseId === courseId
+    );
+    if (data.length === 0) {
+      history.push(`/school/${schoolId}/${subjectId}`);
+      return null;
+    }
+    data = data[0];
+    if (data.sections.length > 0) {
+      data.prerequisite = data.sections[0].prerequisites;
     }
 
-    const getCourseInfo = async() => {
-        let data = await fetchData();
-        setCourseInfo(data);
+    console.log(data);
+
+    response = await axios(`/comments/${courseId}`);
+    data.userReviews = response.data;
+    data.avgRating = 0;
+    if (data.userReviews.length > 0) {
+      data.avgRating = Math.round(
+        data.userReviews.reduce((sum, review) => sum + review.rating, 0) /
+          data.userReviews.length
+      );
     }
 
-    useEffect(() => { getCourseInfo(); }, []);
+    return data;
+  };
 
-    const formatDescriptionText = (description) => {
-        if (description === undefined) return description;
-        if (description.length <= 70) {
-            return <p>Description: {description}</p>;
-        }
-        if (description.length > 70 && showMore) {
-            return (
-                <p>
-                    <a onClick={() => setShowMore(false)}>Description: {description} <i>Show less</i></a>
-                </p>
-            )
-        }
-        if (description.length > 70) {
-            return (
-                <p>
-                    <a onClick={() => setShowMore(true)}>Description: {description.slice(0, 70)} <i>...show more</i></a>
-                </p>
-            )
-        }
-    }
+  const getCourseInfo = async () => {
+    let data = await fetchData();
+    setCourseInfo(data);
+  };
 
-    if (courseInfo === undefined) {
-        return null;
+  const getDay = (date) => {
+    let day = new Date(date);
+    day = day.toString().substring(0, 4);
+    return day;
+  };
+
+  useEffect(() => {
+    getCourseInfo();
+  }, []);
+
+  const formatDescriptionText = (description) => {
+    if (description === undefined) return description;
+    if (description.length <= 70) {
+      return <p>Description: {description}</p>;
     }
-    else {
-        return (
-            <div>
-                <Container fluid className='course-container justify-content-center'>
-                    <Row className='text-center'>
-                        <Col>
-                            <Link to={`/school/${schoolId}/${subjectId}`}><i>Return to Courses</i></Link>
-                        </Col>
-                    </Row>
-                    <Row className='text-center'>
-                        <Col>
-                            <h1>{schoolId + " - " + courseInfo.name}</h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <p className='text-start'>Status: {courseInfo.status ? "Open" : "Closed"}</p>
-                        </Col>
-                        <Col>
-                            <p className='text-end'>Rating: {courseInfo.avgRating}</p>
-                        </Col>
-                    </Row>
-                    <Row className='text-center'>
-                        <Col>
-                            <p>Prerequisite: {courseInfo.prerequisite}</p>
-                        </Col>
-                    </Row>
-                    <Row className='text-center'>
-                        <Col>
-                            {formatDescriptionText(courseInfo.description)}
-                        </Col>
-                    </Row>
-    
-                    {courseInfo.userReviews?.map(review => (
-                        <UserReview key={review.id['$oid']} details={review} />
-                    ))}
-                    
-                </Container>
-            </div>
-        );
+    if (description.length > 70 && showMore) {
+      return (
+        <p>
+          <a onClick={() => setShowMore(false)}>
+            Description: {description} <i>Show less</i>
+          </a>
+        </p>
+      );
     }
+    if (description.length > 70) {
+      return (
+        <p>
+          <a onClick={() => setShowMore(true)}>
+            Description: {description.slice(0, 70)} <i>...show more</i>
+          </a>
+        </p>
+      );
+    }
+  };
+
+  if (courseInfo === undefined) {
+    return null;
+  } else {
+    return (
+      <div>
+        <Container fluid className='course-container justify-content-center'>
+          <Row className='text-center'>
+            <Col>
+              <Link to={`/school/${schoolId}/${subjectId}`}>
+                <i>Return to Courses</i>
+              </Link>
+            </Col>
+          </Row>
+          <Row className='text-center'>
+            <Col>
+              <h1>{schoolId + " - " + courseInfo.name}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className='text-start'>
+                Status: {courseInfo.status ? "Open" : "Closed"}
+              </p>
+            </Col>
+            <Col>
+              <p className='text-end'>Rating: {courseInfo.avgRating}</p>
+            </Col>
+          </Row>
+          <Row className='text-center comment-divider'>
+            <Col>
+              <p>Prerequisite: {courseInfo.prerequisite}</p>
+            </Col>
+          </Row>
+          {courseInfo.sections.map((section) => (
+            <Row key={section.registrationNumber}>
+              <Col xs='12'>Section: {section.code}</Col>
+              <Col xs='12'>
+                Instructor:{" "}
+                {section.instructors.map((instructor) => (
+                  <span key={instructor}>{instructor + " "}</span>
+                ))}
+              </Col>
+              <Col xs='12'>Location: {section.location}</Col>
+              <Col xs='12'>Instruction Mode: {section.instructionMode}</Col>
+              <Col xs='12'>Units: {section.maxUnits}</Col>
+              <Col xs='12'>
+                Dates: {section.meetings[0].beginDate.substring(0, 11)} -{" "}
+                {section.meetings[0].endDate.substring(0, 11)}
+              </Col>
+              <Col xs='12'>
+                Meets: {getDay(section.meetings[0].beginDate.substring(0, 11))}{" "}
+                {section.meetings.length > 1
+                  ? getDay(section.meetings[1].beginDate.substring(0, 11))
+                  : ""}
+              </Col>
+            </Row>
+          ))}
+          <Row className='text-center divider pt-3'>
+            <Col>{formatDescriptionText(courseInfo.description)}</Col>
+          </Row>
+
+          {courseInfo.userReviews?.map((review) => (
+            <UserReview key={review.id["$oid"]} details={review} />
+          ))}
+        </Container>
+      </div>
+    );
+  }
 };
 
 export default CourseInfo;
