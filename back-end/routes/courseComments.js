143658a6e498d9b380362ef938f8f125f85ebad2
PengfanZ <pz723@nyu.edu>
2021-11-24 16:21:20 -0500

    change some front end create-account and login logic

--- back-end/routes/courseComments.js
+++ back-end/routes/courseComments.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 const axios = require('axios');
 // Added as it is required for database intergration
-// const db = require('./db');
+const { Course } = require('../db');
 
 const courseCommentsRouter = Router();
 
 // Just a placeholder to simulate importing the Courses database
 // const { Course } = db;
 
-courseCommentsRouter.get('/:schoolId/:subjectId/:courseId', async(req, res, next) => {
+courseCommentsRouter.get('/:schoolId/:subjectId/:courseId', async (req, res, next) => {
     const { schoolId, subjectId, courseId } = req.params;
     // TODO: get comments from mongodb databases
     axios
@@ -20,11 +20,11 @@ courseCommentsRouter.get('/:schoolId/:subjectId/:courseId', async(req, res, next
         .catch((err) => next(err));
 });
 
-courseCommentsRouter.post('/:courseId', async(req, res, next) => {
+courseCommentsRouter.post('/:courseId', async (req, res, next) => {
     // const { courseId } = req.params;
 
     const id = req.params;
-    const comment = req.body.comment;
+    const { comment } = req.body;
     // TODO: This creating and adding a comment to a Course record which may change based on how the schema is implemented in the database
     // Check that this relationship works
     Course.find({ courseId: id })
